6837b02e002739086c753b8ad2899a8a3ab6d2ed
Michael Wisslead <michael.wisslead@hotmail.com>
2016-05-10 23:40:16 -0500

    Cleaned up field adding method and added more fields.

--- odbcsetup/interface_config.py
+++ odbcsetup/interface_config.py
@@ -140,48 +140,57 @@ def matchup(txt):
     layout.addWidget(layout.box)
     return layout
 
-class MatchPage1(QtGui.QWizardPage):
-    def __init__(self):
+class MatchPage(QtGui.QWizardPage):
+    def __init__(self, fields):
         QtGui.QWizardPage.__init__(self)
         self.setCommitPage(False)
         self.setTitle("Match Fields")
 
         self.layout = QtGui.QVBoxLayout()
-        fields = ['IO', 'ShipComp', 'ShipAttn', 'ShipAddr1', 'ShipAddr2', 'ShipCity', 'ShipState', 'ShipZip']
         self.matchers = {}
-        for field in fields:
-            self.matchers[field] = matchup(field)
+        for field, label in fields:
+            self.matchers[field] = matchup(label)
             self.layout.addLayout(self.matchers[field])
         self.setLayout(self.layout)
 
-    def nextId(self):
-        return -1
-
     def initializePage(self):
+        if not 'matchers' in dir(self.wizard()):
+            self.wizard().matchers = {}
         for key in self.matchers:
+            self.wizard().matchers[key] = self.matchers[key].box
             self.matchers[key].box.clear()
             self.matchers[key].box.addItems([''])
             self.matchers[key].box.addItems(self.wizard().fields)
-
-    def validatePage(self):
-        setupstr = ''
-        fields = self.matchers.keys()
-        for key in fields:
-            setupstr += ';%s C(%d)' % (key, max(len(self.matchers[key].box.currentText()), 1))
-        setupstr = ('type N(3, 0); dsn C(%d); user C(1); pass C(1); table C(%d)' % (len(self.wizard().dsn), len(self.wizard().table))) + setupstr
-        print(setupstr)
-        table = dbf.Table('InterfaceSetup', setupstr)
-        table.open()
-        datum = tuple([1, self.wizard().dsn, '', '', self.wizard().table] + [self.matchers[key].box.currentText() for key in fields])
-        table.append(datum)
-        table.close()
-        return True
 
 def invalidPage():
     page = QtGui.QWizardPage()
     page.setTitle("This function has not been implemented")
     page.isFinalPage = lambda: True
     return page
+
+class LastPage(QtGui.QWizardPage):
+    def __init__(self):
+        QtGui.QWizardPage.__init__(self)
+        self.setCommitPage(False)
+        self.setTitle("Save Settings")
+
+    def nextId(self):
+        return -1
+
+    def validatePage(self):
+        setupstr = ''
+        matchers = self.wizard().matchers
+        fields = matchers.keys()
+        for key in fields:
+            setupstr += ';%s C(%d)' % (key, max(len(matchers[key].currentText()), 1))
+        setupstr = ('type N(3, 0); dsn C(%d); user C(1); pass C(1); table C(%d)' % (len(self.wizard().dsn), len(self.wizard().table))) + setupstr
+        print(setupstr)
+        table = dbf.Table('InterfaceSetup', setupstr)
+        table.open()
+        datum = tuple([1, self.wizard().dsn, '', '', self.wizard().table] + [matchers[key].currentText() for key in fields])
+        table.append(datum)
+        table.close()
+        return True
 
 def main(args=None):
     '''main'''
@@ -193,7 +202,97 @@ def main(args=None):
     wizard.addPage(SourcePage())
     wizard.addPage(TablesPage())
     wizard.addPage(FieldsPage())
-    wizard.addPage(MatchPage1())
+
+    fields = [['IO', 'INTERFACE_PROMPT'],
+              ['PKG_ID', 'PACKAGE_ID'],
+              ['INVC_NO', 'INVOICE_NO'],
+              ['ORD_NO', 'ORDER_NO'],
+              ['PO_NO', 'PO_NO'],
+              ['DEPT_NO', 'DEPT_NO'],
+              ['REF1', 'REF1'],
+              ['REF2', 'REF2'],
+              ['REF3', 'REF3'],
+              ['TRCARCODE', 'TRANSLATED_CARRIER_CODE'],
+              ['TRCTRYCD', 'TRANSLATED_COUNTRY_CODE']]
+    wizard.addPage(MatchPage(fields))
+
+    fields = [['SHIPWT', 'SHIP_WEIGHT'],
+              ['SHIPCUST', 'SHIPTO_CUSTOMER_NO'],
+              ['SHIPCOMP', 'SHIPTO_COMPANY'],
+              ['SHIPATTN', 'SHIPTO_ATTN'],
+              ['SHIPADDR1', 'SHIPTO_ADDR1'],
+              ['SHIPADDR2', 'SHIPTO_ADDR2'],
+              ['SHIPADDR3', 'SHIPTO_ADDR3'],
+              ['SHIPCITY', 'SHIPTO_CITY'],
+              ['SHIPSTATE', 'SHIPTO_STATE'],
+              ['SHIPZIP', 'SHIPTO_ZIP'],
+              ['SHIPCTRY', 'SHIPTO_COUNTRY'],
+              ['SHIPCTRYCD', 'SHIPTO_COUNTRY_CODE'],
+              ['SHIPPHNO', 'SHIPTO_PHONE_NO'],
+              ['SHIPFAXNO', 'SHIPTO_FAX_NO'],
+              ['SHIPEMAIL', 'SHIPTO_EMAIL']]
+    wizard.addPage(MatchPage(fields))
+
+    fields = [['BILLMETHOD', 'BILLING_METHOD'],
+              ['BILLCARACT', 'BILLTO_CARRIER_ACCT_NO'],
+              ['BILLCOMP', 'BILLTO_COMPANY'],
+              ['BILLATTN', 'BILLTO_ATTN'],
+              ['BILLADDR1', 'BILLTO_ADDR1'],
+              ['BILLADDR2', 'BILLTO_ADDR2'],
+              ['BILLADDR', 'BILLTO_ADDR'],
+              ['BILLCITY', 'BILLTO_CITY'],
+              ['BILLSTATE', 'BILLTO_STATE'],
+              ['BILLZIP', 'BILLTO_ZIP'],
+              ['BILLCTRY', 'BILLTO_COUNTRY'],
+              ['BILLCTRYCD', 'BILLTO_COUNTRY_CODE'],
+              ['BILLPHNO', 'BILLTO_PHONE_NO'],
+              ['BILLFAXNO', 'BILLTO_FAX_NO'],
+              ['BILLNO', 'BILLTO_NO']]
+    wizard.addPage(MatchPage(fields))
+
+    fields = [['RETNAME', 'RETURNLABEL_NAME'],
+              ['RETATTN', 'RETURNLABEL_ATTN'],
+              ['RETADDR1', 'RETURNLABEL_ADDR1'],
+              ['RETCITY', 'RETURNLABEL_CITY'],
+              ['RETSTATE', 'RETURNLABEL_STATE'],
+              ['RETZIP', 'RETURNLABEL_ZIP'],
+              ['RETCTRY', 'RETURNLABEL_COUNTRY'],
+              ['RETCTRYCD', 'RETURNLABEL_COUNTRY_CODE'],
+              ['RETPHNO', 'RETURNLABEL_PHONE_NO'],
+              ['RETFAXNO', 'RETURNLABEL_FAX_NO'],
+              ['RETADDR2', 'RETURNLABEL_ADDR2'],
+              ['RETADDR3', 'RETURNLABEL_ADDR3']]
+    wizard.addPage(MatchPage(fields))
+
+    fields = [['COD', 'COD'],
+              ['ADDTOCOD', 'ADDTO_COD'],
+              ['CLCTCOD', 'COLLECT_COD'],
+              ['INSUR', 'INSURANCE'],
+              ['SATDELIV', 'SAT_DELIVERY'],
+              ['SATPICKUP', 'SAT_PICKUP'],
+              ['DIMNSNL', 'DIMENSIONAL'],
+              ['RSDNTL', 'RESIDENTIAL'],
+              ['CODAMNT', 'COD_AMOUNT'],
+              ['CODCHRG', 'COD_CHARGE'],
+              ['INSURAMNT', 'INSURANCE_AMOUNT'],
+              ['INSURCHRG', 'INSURANCE_CHARGE']]
+    wizard.addPage(MatchPage(fields))
+
+    fields = [['CALLTGCHRG', 'CALL_TAG_CHARGE'],
+              ['DIMLENGTH', 'DIMENSIONAL_LENGTH'],
+              ['DIMWIDTH', 'DIMENSIONAL_WIDTH'],
+              ['DIMHEIGHT', 'DIMESIONAL_HEIGHT'],
+              ['DIMWEIGHT', 'DIMENSIONAL_WEIGHT'],
+              ['MISC1', 'MISC1'],
+              ['MISC2', 'MISC2'],
+              ['MISC3', 'MISC3'],
+              ['MISC4', 'MISC4'],
+              ['RBRSTMP1', 'RUBBER_STAMP1'],
+              ['RBRSTMP2', 'RUBBER_STAMP2'],
+              ['RBRSTMP3', 'RUBBER_STAMP3']]
+    wizard.addPage(MatchPage(fields))
+
+    wizard.addPage(LastPage())
     wizard.addPage(invalidPage())
 
     wizard.setWindowTitle("Interface Setup Wizard")
